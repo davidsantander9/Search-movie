b12f5de5cd095a893a577441768a4d89b7b9da34
macbook <macbook@MacBook-Pro-de-macbook.local>
2020-03-12 17:38:32 -0600

    Add SearchForm component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 
-import {Title} from './components/Title'
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <Title>Search Movies</Title>
         <img src={logo} className="App-logo" alt="logo" />
+        <SearchForm></SearchForm>
       </header>
     </div>
   );
