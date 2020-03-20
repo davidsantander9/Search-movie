6e030c4d5d64c77faa49d2b1dc72112593ae424a
macbook <macbook@MacBook-Pro-de-macbook.local>
2020-03-19 19:03:03 -0600

    Updates

---
+++ precache-manifest.28499944334764b194f0a2c573be1d24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed5064ce28ae29ab8fb86ad44d1c2b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "587df5d66b22fd2d08ea",
+    "url": "/static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "250f5e1b9503e90aed37",
+    "url": "/static/css/main.374007c0.chunk.css"
+  },
+  {
+    "revision": "587df5d66b22fd2d08ea",
+    "url": "/static/js/2.1d12fc76.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1d12fc76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250f5e1b9503e90aed37",
+    "url": "/static/js/main.cf175065.chunk.js"
+  },
+  {
+    "revision": "ec749b6198e2bf84ea37",
+    "url": "/static/js/runtime-main.06fcd387.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
